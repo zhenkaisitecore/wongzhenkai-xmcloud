ef1e186725bebb1cd5b791b72d908095f4fb1291
zhe <zhenkai.wong@sitecore.com>
2023-12-07 12:15:30 +0800

    Build rendering

--- src/sxastarter/src/components/TestRendering.tsx
+++ src/sxastarter/src/components/TestRendering.tsx
@@ -3,37 +3,23 @@ import React from 'react';
 import { Field, Text } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
-
   SomeText: Field<string>;
-
-} 
+}
 
 type MyRenderingProps = {
-
   params: { [key: string]: string };
 
   fields: Fields;
-
 };
 
 export const Default = (props: MyRenderingProps): JSX.Element => {
-
   return (
-
     <div className={`component myrendering ${props.params.styles}`}>
-
       <div className="component-content">
-
         <div>
-
           <Text field={props.fields.SomeText} />
-
         </div>
-
       </div>
-
     </div>
-
   );
-
 };
