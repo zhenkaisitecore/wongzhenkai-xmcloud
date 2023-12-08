36981a8d560e2dedc8f05a4ce71bd18af995f7fc
zhe <zhenkai.wong@sitecore.com>
2023-12-08 16:34:55 +0800

    Remove custom components

--- src/sxastarter/src/custom-components/index.tsx
+++
@@ -1,6 +0,0 @@
-
-import * as FEAAS from "@sitecore-feaas/clientside/react";
-import ClientsideBundle from "./index.client";
-import dynamic from "next/dynamic";
-
-FEAAS.enableNextClientsideComponents(dynamic, ClientsideBundle);
